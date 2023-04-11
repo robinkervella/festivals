64f3b8be0ce832712c15e5a93cc393863e7d052a
BenjaminGetin <ggetin.benjamin@gmail.com>
2023-04-11 14:32:23 +0200

    json raté

--- src/main/java/fr/simplon/festivals/controller/FestivalController.java
+++ src/main/java/fr/simplon/festivals/controller/FestivalController.java
@@ -15,14 +15,15 @@ import java.util.List;
 
 @Controller
 public class FestivalController {
+
     @Autowired
     private FestivalDao festivalDao;
 
     @GetMapping("/")
     public String afficherTousLesFestivals(Model model) {
         List<Festival> festivals = festivalDao.getAllFestivals();
-        model.addAttribute("festivals", festivals);
-        return "festivals";
+        model.addAttribute("festivals", festivals); // corrected attribute name
+        return "index";
     }
 
     @GetMapping("/create")
@@ -31,30 +32,22 @@ public class FestivalController {
         return "create";
     }
 
-    @PostMapping ("/ajouterFestival")
+    @PostMapping("/ajouterFestival")
     public String enregistrerFestival(@ModelAttribute("festival") Festival festival) {
         festivalDao.saveFestival(festival);
         return "redirect:/";
     }
 
-    @PostMapping ("/submit")
-    public String submitForm (@RequestParam("nom") String nom,
-                              @RequestParam("url") String url,
-                              @RequestParam("debut") LocalDate debut,
-                              @RequestParam("fin") LocalDate fin,
-                              @RequestParam("ville") String ville, @RequestParam("cp") int cp,
-                              @RequestParam("lieu") String lieu, @RequestParam("lat") double lat,
-                              @RequestParam("lon") double lon,
+    @PostMapping("/submit")
+    public String submitForm (@ModelAttribute("festival") Festival festival, Model model) {
+        //Enregistrer le festival dans la base de données
+        festivalDao.saveFestival(festival);
 
-                              Model model) {
+        //Ajouter le festival à la liste pour l'afficher dans la vue index
+        List<Festival> festivals = festivalDao.getAllFestivals();
+        model.addAttribute("festivals", festivals);
 
-        //Ajout des données dans le modèle
-
-
-        Festival festival = new Festival();
-        model.addAttribute("festival", festival);
-
-        //Retourne la vue qui affichera les données du formulaire
-        return "result";
+        //Retourne la vue index qui affichera la liste des festivals
+        return "index";
     }
 }
