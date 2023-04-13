c4b80989a3a809e95af8d966c7667f2cb5da0e51
BenjaminGetin <ggetin.benjamin@gmail.com>
2023-04-13 10:42:37 +0200

    merci pour la javadoc robin

--- src/main/java/fr/simplon/festivals/controller/FestivalController.java
+++ src/main/java/fr/simplon/festivals/controller/FestivalController.java
@@ -6,17 +6,25 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-
-import java.time.LocalDate;
 import java.util.List;
-import java.util.Optional;
 
+/**
+ Cette classe est un contrôleur Spring MVC qui gère les requêtes liées aux festivals.
+ */
 @Controller
 public class FestivalController {
-
+    /**
+     * Le DAO pour accéder aux données de festival.
+     */
     @Autowired
     private FestivalDao festivalDao;
 
+    /**
+     * Cette méthode permet d'afficher tous les festivals sur la page d'accueil.
+     *
+     * @param model le modèle qui sera utilisé pour stocker les festivals
+     * @return le nom de la vue qui affiche la page d'accueil
+     */
     @GetMapping("/")
     public String afficherTousLesFestivals(Model model) {
         List<Festival> festivals = festivalDao.getAllFestivals();
@@ -24,13 +32,25 @@ public class FestivalController {
         return "index";
     }
 
+    /**
+     * Cette méthode permet d'afficher le formulaire de création d'un festival.
+     *
+     * @param model le modèle qui sera utilisé pour stocker le festival à créer
+     * @return le nom de la vue qui affiche le formulaire de création d'un festival
+     */
     @GetMapping("/create")
     public String afficherFormulaire(Model model) {
         model.addAttribute("festival", new Festival());
         return "create";
     }
 
-
+    /**
+     * Cette méthode permet de modifier un festival existant.
+     *
+     * @param id    l'identifiant du festival à modifier
+     * @param model le modèle qui sera utilisé pour stocker le festival à modifier
+     * @return le nom de la vue qui affiche le formulaire de modification d'un festival
+     */
     @GetMapping("/edit/{id}")
     public String editFestival(@PathVariable("id") Long id, Model model) {
         Festival festival = festivalDao.findById(id)
@@ -39,6 +59,11 @@ public class FestivalController {
         return "edit";
     }
 
+    /**
+     * Cette méthode permet de récupérer tous les festivals au format JSON.
+     *
+     * @return la liste des festivals au format JSON
+     */
     @GetMapping("/api/festivals")
     @ResponseBody
     public List<Festival> getAllFestivalsAsJson() {
@@ -46,24 +71,30 @@ public class FestivalController {
         return festivals;
     }
 
-
+    /**
+     * Cette méthode permet d'enregistrer un festival créé.
+     *
+     * @param festival le festival à enregistrer
+     * @return le nom de la vue qui affiche la page d'accueil
+     */
     @PostMapping("/ajouterFestival")
     public String enregistrerFestival(@ModelAttribute("festival") Festival festival) {
-        if(festival.getId() == null){
-        // c'est un nouveau festival, donc il faut l'enregistrer dans la base de données
+        if (festival.getId() == null) {
+            // c'est un nouveau festival, donc il faut l'enregistrer dans la base de données
             festivalDao.saveFestival(festival);
         } else {
-        // c'est un festival existant, donc il faut le mettre à jour dans la base de données
+            // c'est un festival existant, donc il faut le mettre à jour dans la base de données
             festivalDao.updateFestival(festival);
         }
         return "redirect:/";
     }
+
 
     @PostMapping("/editerFestival")
     public String editerFestival(@ModelAttribute("festival") Festival festival, @PathVariable("id") Long id) {
         festival.setId(id);
         festivalDao.updateFestival(festival);
         return "redirect:/";
-    }
 
+    }
 }
--- src/main/java/fr/simplon/festivals/dao/impl/FestivalDaoImpl.java
+++ src/main/java/fr/simplon/festivals/dao/impl/FestivalDaoImpl.java
@@ -7,12 +7,22 @@ import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
-
+/**
+ Cette classe est une implémentation de l'interface FestivalDao
+ pour accéder aux données de festival en utilisant
+ un objet FestivalRepository.
+ */
 @Repository
 public class FestivalDaoImpl implements FestivalDao {
+    /**
+     Le repository utilisé pour accéder aux données de festival.
+     */
     @Autowired
     private FestivalRepository festivalRepository;
-
+    /**
+     Enregistre un festival dans la base de données.
+     @param festival le festival à enregistrer
+     */
     @Override
     public void saveFestival(Festival festival) {
         festivalRepository.save(festival);
@@ -24,10 +34,19 @@ public class FestivalDaoImpl implements FestivalDao {
     }
 
 
+    /**
+     Récupère la liste de tous les festivals enregistrés dans la base de données.
+     @return la liste de tous les festivals
+     */
     @Override
     public List<Festival> getAllFestivals() {
         return festivalRepository.findAll(); }
 
+    /**
+     Recherche un festival dans la base de données par son identifiant.
+     @param id l'identifiant du festival à rechercher
+     @return un objet Optional contenant le festival si l'identifiant est valide, sinon Optional.empty()
+     */
     @Override
     public Optional<Festival> findById(Long id) {
         return festivalRepository.findById(id);
